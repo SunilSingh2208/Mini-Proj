21f7f79673405af8e005c7fb6d7d94b770b875f3
SunilSingh2208 <sunilsingh39943@gmail.com>
2025-04-28 00:31:47 +0530

    Second commit

--- mini-Proj/src/App.jsx
+++ mini-Proj/src/App.jsx
@@ -9,7 +9,7 @@ function App(){
     <>
       <h1>This is a first vite-react App...</h1>
       <p>Under there is a button that changes the value of count on click...</p>
-      <p>hello there</p>
+      <p>hey there</p>
       <button onClick={()=>{setCount(count+1)}}>Count {count}</button>
     </>
   )

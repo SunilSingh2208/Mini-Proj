a8da3b0f4949e61c3af1541187cc32568c973d16
SunilSingh2208 <sunilsingh39943@gmail.com>
2025-04-28 15:42:57 +0530

    Set two counters

--- mini-Proj/src/App.jsx
+++ mini-Proj/src/App.jsx
@@ -1,18 +1,27 @@
 import {useState} from 'react';
-import './App.css';
-
-function App(){
-
-  const[count,setCount] = useState(0);
+export default function App(){
 
   return(
     <>
-      <h1>This is a first vite-react App...</h1>
-      <p>Under there is a button that changes the value of count on click...</p>
-      <p>hey there</p>
-      <button onClick={()=>{setCount(count+1)}}>Count {count}</button>
+      <h1>This is a mini-vite app...</h1>
+      <MyButton/>
+      <MyButton/>
     </>
   )
 }
-export default App
 
+function MyButton(){
+
+  const [counter, SetCounter] = useState(0);
+
+  function handleClick(){
+      SetCounter(counter + 1);
+  }
+
+  return(
+
+    <>
+      <button onClick={handleClick}>Count {counter}</button>
+    </>
+  )
+}

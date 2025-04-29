1fba8293f693b59b7d866d8baf28d4e6177a2f89
SunilSingh2208 <sunilsingh39943@gmail.com>
2025-04-29 10:18:50 +0530

    Two counts changing simultaneously

--- mini-Proj/src/App.jsx
+++ mini-Proj/src/App.jsx
@@ -1,28 +1,59 @@
+// import {useState} from 'react';
+// export default function App(){
+
+//   return(
+//     <>
+//       <h1>This is a mini-vite app...</h1>
+//       <p>counters that update separately..</p>
+//       <MyButton/>
+//       <MyButton/>
+//     </>
+//   )
+// }
+
+// function MyButton(){
+
+//   const [counter, SetCounter] = useState(0);
+
+//   function handleClick(){
+//       SetCounter(counter + 1);
+//   }
+
+//   return(
+
+//     <>
+//       <button onClick={handleClick}>Count {counter}</button>
+//     </>
+//   )
+// }
+
+
 import {useState} from 'react';
+
 export default function App(){
 
-  return(
-    <>
-      <h1>This is a mini-vite app...</h1>
-      <p>counters that update separately..</p>
-      <MyButton/>
-      <MyButton/>
-    </>
-  )
-}
-
-function MyButton(){
-
-  const [counter, SetCounter] = useState(0);
+  const [count, setCount] = useState(0);
 
   function handleClick(){
-      SetCounter(counter + 1);
+    setCount(count + 1);
+
   }
 
   return(
+    <>    <h1>Two counts..</h1>
+    <p>These counts will run simultaneously</p>
+    <MyButton count={count} onClick={handleClick}/>
+    <MyButton count={count} onClick={handleClick}/>
+    </>
 
+  )
+
+}
+
+function MyButton({count,onClick}){
+  return(
     <>
-      <button onClick={handleClick}>Count {counter}</button>
+    <button onClick={onClick}>Count {count}</button>
     </>
   )
 }
